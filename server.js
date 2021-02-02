0fdb15f5dc8ecbd1612c2a55fc2620bd90f1c36d
Stephen Price <stephen_lprice@live.com>
2021-02-02 01:28:08 -0600

    fixed bug caused by '*' path from homepage blocking static files from being served.

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('./public'));
 
 // Routes
 // Home page
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
 
 // Notes page
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));

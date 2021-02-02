0a067bed5095760bfa595d6b0664716ed205d574
Stephen Price <stephen_lprice@live.com>
2021-02-02 02:45:35 -0600

    deleting notes

--- server.js
+++ server.js
@@ -41,6 +41,20 @@ app.delete('/api/notes/:id', (req, res) => {
     // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. 
     // In order to delete a note, you'll need to read all notes from the db.json file, remove the note 
     // with the given id property, and then rewrite the notes to the db.json file.
+    const id = req.params.id;
+    let index = undefined;
+    console.log("DELETE Request called for note with ID: " + id);
+
+    for (let i = 0; i < db.length; i++) {
+        console.log(db[i]);
+        if (id === db[i].id) {
+            index = db.indexOf(db[i]);
+            console.log("deleting note with ID: " + db[i].id);
+            db.splice(index, 1);
+            console.log(db);
+            return res.json(db);
+        }
+    }
 });
 
 // Starts the server to begin listening
